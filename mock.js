dd55b87fa5119f9212b7ed4e560382b9358daffd
boris <bvpinus@gmail.com>
2019-07-15 02:09:04 +0300

    mock circles, forces and popover base

---
+++ mock.js
@@ -0,0 +1,85 @@
+const totalData = [
+  {
+    name: "home",
+    number: 47,
+    sentencedPercentage: 70,
+    type: 'total'
+  },
+  {
+    name: "work",
+    number: 13,
+    sentencedPercentage: 20,
+    type: 'total'
+  },
+  {
+    name: "yard",
+    number: 4,
+    sentencedPercentage: 16,
+    type: 'total'
+  },
+  {
+    name: "street",
+    number: 50,
+    sentencedPercentage: 18,
+    type: 'total'
+  }
+];
+
+const onlineData = [
+  {
+    name: "home",
+    number: 10,
+    sentencedPercentage: 80,
+    type: 'online',
+  },
+  {
+    name: "work",
+    number: 5,
+    sentencedPercentage: 30,
+    type: 'online',
+  },
+  {
+    name: "yard",
+    number: 4,
+    sentencedPercentage: 56,
+    type: 'online',
+  },
+  {
+    name: "street",
+    number: 40,
+    sentencedPercentage: 11,
+    type: 'online',
+  }
+];
+const offlineData = [
+  {
+    name: "home",
+    number: 37,
+    sentencedPercentage: 0,
+    type: 'offline',
+  },
+  {
+    name: "work",
+    number: 8,
+    sentencedPercentage: 93,
+    type: 'offline',
+  },
+  {
+    name: "yard",
+    number: 0,
+    sentencedPercentage: 0,
+    type: 'offline',
+  },
+  {
+    name: "street",
+    number: 10,
+    sentencedPercentage: 20,
+    type: 'offline',
+  }
+];
+
+export {
+  totalData,
+  offlineData,
+  onlineData
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,124 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Chart from './components/Chart'
+import * as d3 from "d3";
+import { totalData, offlineData, onlineData } from "../mock";
 
-class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Chart />
-      </React.Fragment>
-    );
-  }
-}
+import "./index.css";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const data = {
+  total: totalData,
+  byType: offlineData.concat(onlineData)
+};
+
+const WIDTH = 0.8 * window.innerWidth;
+const HEIGHT = 0.8 * window.innerHeight;
+const MAX_RADIUS = 100;
+
+const initialType = "total";
+const t = d3.transition().duration(1500);
+
+const svg = d3
+  .select(".chart-svg")
+  .attr("width", WIDTH)
+  .attr("height", HEIGHT);
+
+const g = svg
+  .append("g")
+  .attr("transform", `translate(${WIDTH / 2}, ${HEIGHT / 2})`);
+
+const radiusScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(data[initialType], d => d.number)])
+  .range([0, MAX_RADIUS]);
+
+const ticked = () => {
+  d3.selectAll(".bubble").attr("transform", d => {
+    return `translate(${d.x}, ${d.y})`;
+  });
+};
+
+const simulation = d3
+  .forceSimulation(data[initialType])
+  .force("charge", d3.forceManyBody().strength(-15))
+  .force("forceX", d3.forceX().strength(0.05))
+  .force("forceY", d3.forceY().strength(0.05))
+  .force("center", d3.forceCenter())
+  .on("tick", ticked);
+
+d3.select(".decade-btn").on("click", () => {
+  render("byType");
+});
+
+d3.select(".combine-btn").on("click", () => {
+  render("total");
+});
+
+const render = type => {
+  const bubbles = g.selectAll(".bubble").data(data[type]);
+
+  const bubblesEnter = bubbles
+    .enter()
+    .append("g")
+    .on("mouseover", function() {
+      d3.select(this).raise();
+      d3.select(this)
+        .select(".popover")
+        .style("display", null);
+    })
+    .on("mouseout", function() {
+      d3.select(this)
+        .select(".popover")
+        .style("display", "none");
+    })
+    .attr("class", "bubble");
+
+  bubbles
+    .exit()
+    .transition()
+    .attr("transform", "translate(-300, 0)")
+    .duration(200)
+    .remove();
+
+  bubblesEnter
+    .append("circle")
+    .merge(bubbles.select("circle"))
+    .transition(t)
+    .attr("transform", d => {
+      let x = -300;
+      if (d.type === "total") {
+        x = 0;
+      } else if (d.type === "online") {
+        x = 300;
+      }
+      return `translate(${x}, 0)`;
+    })
+    .attr("r", ({ number }) => radiusScale(number))
+    .attr("fill", "#C7A28F")
+    .duration(200);
+
+  bubblesEnter
+    .append("rect")
+    .merge(bubbles.select("rect"))
+    .transition(t)
+    .attr("transform", d => {
+      let x = -300;
+      if (d.type === "total") {
+        x = 0;
+      } else if (d.type === "online") {
+        x = 300;
+      }
+      return `translate(${x}, 0)`;
+    })
+    .attr("class", "popover")
+    .style("display", "none")
+    .attr("width", 200)
+    .attr("height", 100)
+    .attr("stroke", "#1a1f01")
+    .attr("fill", "#fff")
+    .duration(200);
+
+  simulation
+    .nodes(data[type])
+    .force("collide", d3.forceCollide(({ number }) => radiusScale(number) + 2))
+    .alphaTarget(1);
+};
+
+render("total");
